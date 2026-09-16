b6ebf16a655016c2174eed0706a76b288ffcd760
PequenoJ10 <josecuentapeliculas@gmail.com>
2026-09-16 17:41:26 -0500

    Actualizar api.js con la URL de producción de Render

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://futpredict-4ei2.onrender.com/api";
 
 document.addEventListener("DOMContentLoaded", () => {
     loadMatches();
